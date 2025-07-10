dfb355610bc9e6c8ebe8fbe286742dba9eea7c8d
Sibiya07 <134922789+Sibiya07@users.noreply.github.com>
2025-07-10 21:45:43 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,12 @@
 require('dotenv').config()
+const express = require('express');
+const cors = require('cors'); // ✅ Add this line
 const app = require('./src/app')
 
-
+// If you’re applying CORS here (but better to do it inside app.js)
+app.use(cors({
+  origin: "https://codereview-1-dw6w.onrender.com"
+}));
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000')
